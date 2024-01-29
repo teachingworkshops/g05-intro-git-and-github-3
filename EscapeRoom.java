2c047cd5f0cae99b9844fd33991cd49a4135b164
deandradejatwit <70961399+deandradejatwit@users.noreply.github.com>
2024-01-29 13:59:31 -0500

    updated escaperoom and player

--- EscapeRoom.java
+++ EscapeRoom.java
@@ -1,20 +1,14 @@
 
 public class EscapeRoom {
-    private boolean win;
-    private Player player;
-
-    private Point startPosition = new Point(0, 0);
-
     public EscapeRoom(){
-        this.player = new Player(startPosition);
-        this.win = false;
+
     }
 
-    public void exitConsolInteract(){
-        if(win){
-            System.out.println("Pass: Please enter the keypad code");
-        } else {
-            System.out.println("Error: You have not found all keypad values");
-        }
+    public void initializeMatrix(){
+        
+    }
+
+    public void roomCheck(){
+
     }
 }
--- Player.java
+++ Player.java
@@ -2,22 +2,16 @@ import java.util.ArrayList;
 
 public class Player {
     private ArrayList <String> inventory;
-    private Point point;
 
-    public Player (Point p) {
-        this.inventory = new ArrayList<>();
-        this.point = p;
+    public Player () {
+
+    }
+    
+    public void updateInventory(){
+
     }
 
-    public Point getPosition(){
-        return point;
-    }
-
-    public void pickUpItem(String item){
-        inventory.add(item);
-    }
-
-    public void listInventory(){
+    public void printInventory(){
         for (String item : inventory){
             System.out.print(item);
         }
