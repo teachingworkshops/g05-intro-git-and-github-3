74ebf80b33263b93722d366c38ae2d6895fd8ef9
deandradejatwit <70961399+deandradejatwit@users.noreply.github.com>
2024-01-29 13:15:07 -0500

    removed player movement

--- EscapeRoom.java
+++ EscapeRoom.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class EscapeRoom {
     private boolean win;
@@ -10,26 +8,6 @@ public class EscapeRoom {
     public EscapeRoom(){
         this.player = new Player(startPosition);
         this.win = false;
-    }
-
-    public void movePlayer(String move){
-        move.toLowerCase();
-
-        // still need to add in out of bound checks; 
-
-        if (move.equals("up")) {
-            int newRow = player.getPosition().getRow() + 1;
-            player.movePosition(newRow, player.getPosition().getColumn());
-        } else if ((move.equals("right"))) {
-            int newCol = player.getPosition().getColumn() + 1;
-            player.movePosition(player.getPosition().getColumn(),newCol);
-        } else if ((move.equals("left"))) {
-            int newCol = player.getPosition().getColumn() - 1;
-            player.movePosition(player.getPosition().getColumn(),newCol);
-        } else if ((move.equals("down"))){
-            int newRow = player.getPosition().getRow() - 1;
-            player.movePosition(newRow, player.getPosition().getColumn());
-        } 
     }
 
     public void exitConsolInteract(){
