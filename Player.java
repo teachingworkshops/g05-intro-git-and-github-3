f9110bd354309893e58529f698ff2c8d3a848e6a
sullivanm22 <sullivanm22@wit.edu>
2024-01-30 15:36:20 -0500

    added documentation

--- Player.java
+++ Player.java
@@ -3,26 +3,33 @@ import java.util.ArrayList;
 public class Player {
 
     private ArrayList <String> inventory;
+    //sets players intial starting position to 2,1
     private Point position = new Point(2, 1);
 
-    public Player () {
-
+    //basic player constructor
+    public Player(){
     }
     
+    //method that must be called by EscapeRoom roomCheck() if player finds a key
+    //adds a string to the arraylist inventory 
     public void updateInventory(){
 
     }
 
+    //method used to print current inventory
     public void printInventory(){
+        System.out.println("Inventory:");
         for (String item : inventory){
-            System.out.print(item);
+            System.out.println(item);
         }
     }
 
+    //method for accessing player's current position throughout other classes useful for roomCheck()
     public Point getPosition(){
         return this.position;
     }
 
+    //method for updating player's position used in every move function
     public void setPosition(int row, int col){
         this.position.setRow(row);
         this.position.setColumn(col);
