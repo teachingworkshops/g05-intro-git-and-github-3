1be97d823a961d80323ad47287f360f7e995ddc2
sullivanm22 <sullivanm22@wit.edu>
2024-01-30 18:15:56 -0500

    updated move methods to no longer change room values

--- Move.java
+++ Move.java
@@ -4,44 +4,27 @@ public class Move {
         if (row == 0) {
             System.out.println("You hit a wall, you can't move there.");
         }
-        room[row][col] = 0;
-        room[row-1][col] = 1;
-        row++;
-
-        player.setPosition(row, col);
+        player.setPosition(row--, col);
     }
     
     public static void moveDown(int[][] room,Player player, int row, int col) {
         if (row == 3) {
             System.out.println("You hit a wall, you can't move there.");
         }
-    
-        room[row][col]=0;
-        room[row+1][col] = 1;
-        row++;
-        
-        player.setPosition(row, col);
+        player.setPosition(row++, col);
     }
     
     public static void moveLeft(int[][] room,Player player, int row, int col) {
         if (col == 0) {
             System.out.println("You hit a wall, you can't move there.");
         }
-        room[row][col]=0;
-        room[row][col-1] = 1;
-        col--;
-
-        player.setPosition(row, col);
+        player.setPosition(row, col--);
     }
 
     public static void moveRight(int[][] room,Player player, int row, int col){
         if (col == 3) {
             System.out.println("You hit a wall, you can't move there.");
         }
-        room[row][col]=0;
-        room[row][col+1]=1;
-        col++;
-
-        player.setPosition(row, col);
+        player.setPosition(row, col++);
 }
 }
