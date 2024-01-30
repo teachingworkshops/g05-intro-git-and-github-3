4893c44a71827c53a335c7c5dbfd13bbffc34711
deandradejatwit <70961399+deandradejatwit@users.noreply.github.com>
2024-01-30 18:22:25 -0500

    updated Move to work as a if else when checking out of bounds

--- Move.java
+++ Move.java
@@ -2,29 +2,33 @@ public class Move {
 
     public static void moveUp(int[][] room, Player player, int row, int col){
         if (row == 0) {
-            System.out.println("You hit a wall, you can't move there.");
+            System.out.println("You hit a wall, you can't move there.");  
+        } else {
+            player.setPosition(row--, col);
         }
-        player.setPosition(row--, col);
     }
     
     public static void moveDown(int[][] room,Player player, int row, int col) {
         if (row == 3) {
             System.out.println("You hit a wall, you can't move there.");
+        } else {
+            player.setPosition(row++, col);   
         }
-        player.setPosition(row++, col);
     }
     
     public static void moveLeft(int[][] room,Player player, int row, int col) {
         if (col == 0) {
             System.out.println("You hit a wall, you can't move there.");
+        } else {
+            player.setPosition(row, col--);
         }
-        player.setPosition(row, col--);
     }
 
-    public static void moveRight(int[][] room,Player player, int row, int col){
+    public static void moveRight(int[][] room, Player player, int row, int col){
         if (col == 3) {
             System.out.println("You hit a wall, you can't move there.");
+        } else {
+            player.setPosition(row, col++);
         }
-        player.setPosition(row, col++);
 }
 }
