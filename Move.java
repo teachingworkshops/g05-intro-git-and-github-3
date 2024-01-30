0c472133c518ac5235a5fee85970dd78c6f77387
deandradejatwit <70961399+deandradejatwit@users.noreply.github.com>
2024-01-30 17:58:31 -0500

    added player position update and change matrix

--- Move.java
+++ Move.java
@@ -1,38 +1,47 @@
 public class Move {
 
-    public static void moveUp(int[][] position, int row, int col){
+    public static void moveUp(int[][] room, Player player, int row, int col){
         if (row == 0) {
             System.out.println("You hit a wall, you can't move there.");
         }
-        position[row][col] = 0;
-        position[row-1][col] = 1;
-        row = row-1;
+        room[row][col] = 0;
+        room[row-1][col] = 1;
+        row++;
+
+        player.setPosition(row, col);
     }
     
-    public static void moveDown(int[][] position, int row, int col) {
+    public static void moveDown(int[][] room,Player player, int row, int col) {
         if (row == 3) {
             System.out.println("You hit a wall, you can't move there.");
         }
     
-        position[row][col]=0;
-        position[row+1][col] = 1;
-        row=row+1;
+        room[row][col]=0;
+        room[row+1][col] = 1;
+        row++;
         
+        player.setPosition(row, col);
     }
     
-    public static void moveLeft(int[][] position, int row, int col) {
+    public static void moveLeft(int[][] room,Player player, int row, int col) {
         if (col == 0) {
             System.out.println("You hit a wall, you can't move there.");
         }
-        position[row][col]=0;
-        position[row][col-1] = 1;
+        room[row][col]=0;
+        room[row][col-1] = 1;
+        col--;
+
+        player.setPosition(row, col);
     }
 
-    public static void moveRight(int[][] position, int row, int col){
+    public static void moveRight(int[][] room,Player player, int row, int col){
         if (col == 3) {
             System.out.println("You hit a wall, you can't move there.");
         }
-        position[row][col]=0;
-        position[row][col+1]=1;
+        room[row][col]=0;
+        room[row][col+1]=1;
+        col++;
+
+        player.setPosition(row, col);
 }
 }
